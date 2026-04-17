6b26462146952a89350423a4d13a7b74fcb8e266
alexaugustobr <alexaugustoti@gmail.com>
2026-04-17 11:46:04 -0300

    projeto inicial

---
+++ src/main/java/com/algaworks/oauth2/example/algashop/oauth2_client_backend_spring_web_app/Oauth2ClientBackendSpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.oauth2.example.algashop.oauth2_client_backend_spring_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientBackendSpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ClientBackendSpringWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/oauth2/example/algashop/oauth2_client_backend_spring_web_app/Oauth2ClientBackendSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.oauth2.example.algashop.oauth2_client_backend_spring_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientBackendSpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
